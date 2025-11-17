521b255c7386a762226cd18e16f8b0223c68ebeb
Barcelo Emmanuel <emmanuelbarcelo22@gmail.com>
2025-11-17 16:17:55 -0300

    Continuacion Modelo Vista Controlador

--- tp_integrador_back/index.js
+++ tp_integrador_back/index.js
@@ -3,18 +3,37 @@ const app = express();
 import environments from "./src/api/config/environments.js"; 
 const PORT = environments.port;
 import cors from "cors"; 
-import { loggerUrl, validateId } from "./src/api/middlewares/middlewares.js";
+import { loggerUrl } from "./src/api/middlewares/middlewares.js";
 import { productRoutes } from "./src/api/routes/index.js";
+
+import { __dirname, join } from "./src/api/utils/index.js"
+import connection from "./src/api/database/db.js";
 
 app.use(cors()); 
 app.use(express.json()); 
 
 app.use(loggerUrl);
 
+app.use(express.static(join(__dirname, "src/public")));
+
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "src/views"));
+
 app.use("/api/products", productRoutes);
 
-
-
+app.get("/dashboard", async (req, res) => {
+    try{
+        const [rows] = await connection.query("SELECT * FROM productos");
+        
+        res.render("index", {
+        title: "Dashboard",
+        about: "Listado de productos",
+        productos: rows
+    });
+    } catch(error){
+        console.error(error);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
